98b3dd46f6f39675220973a07682951bdde72d7e
animation-VKGlobal <animation@vkpublications.com>
2026-01-09 11:30:58 +0000

    dataTypes is saved now

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,7 @@
+let Name = "Praveen" // String data type
+let age = 25 // Number data type
+let isStudent = true // Boolean data type
+let address = null // Null data type
+let phoneNumber
+
+console.log(typeof Name) // Output: string

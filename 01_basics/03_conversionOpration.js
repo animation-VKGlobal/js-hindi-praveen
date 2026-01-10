1eb2a949522ae26fdfdc1bd993425e96b151cc4c
animation-VKGlobal <animation@vkpublications.com>
2026-01-10 09:41:56 +0000

    oprational done

--- 01_basics/03_conversionOpration.js
+++ 01_basics/03_conversionOpration.js
@@ -30,9 +30,42 @@ let booleanIsloggedIn = Boolean(isloggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber)
-console.log(stringNumber)
+//console.log(typeof stringNumber)
+//console.log(stringNumber)
 
 // 33 ==> "33"
 // true ==> "true"
 // false ==> "false"
+
+//*****************Operations in JavaScript ***************/
+
+let value = 3
+let nagValue = -value
+//console.log(nagValue)
+
+let str1 = "Hello"
+let str2 = "World"
+
+//console.log(str1 + " " + str2)
+//console.log(str1 + 5)
+//console.log(str1 - 5) //NaN
+
+// + operator is used for concatenation if any one operand is a string
+// - , * , / operators are not defined for strings
+
+// console.log(5 + "5") // "55"
+// console.log("5" - 3) // 2
+// console.log("5" * "2") // 10
+// console.log("10" / "2") // 5
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"

6461ec4ac5bd2c7cffcda7224074754d01935f5b
animation-VKGlobal <animation@vkpublications.com>
2026-01-10 09:19:39 +0000

    conversion of datatyple

--- 01_basics/03_conversionOpration.js
+++ 01_basics/03_conversionOpration.js
@@ -1,3 +1,38 @@
-let score = 33
+let score = "33abc"
 
-console.log(typeof score)
+//console.log(typeof score)
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+// "33" ==> 33
+// "33abc" ==> NaN
+// true ==> 1
+// false ==> 0
+// "true" ==> NaN
+// "" ==> 0
+
+let isloggedIn = 1
+
+let booleanIsloggedIn = Boolean(isloggedIn)
+//console.log(typeof booleanIsloggedIn)
+//console.log(booleanIsloggedIn)
+
+// 1 ==> true
+// 0 ==> false
+// "abc" ==> true
+// "" ==> false
+// null ==> false
+// undefined ==> false
+// NaN ==> false
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
+console.log(stringNumber)
+
+// 33 ==> "33"
+// true ==> "true"
+// false ==> "false"
